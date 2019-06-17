05bfc04e2a7b4292aee4f9814f4dc126fa1d91ad
eomjeongho <eomjeongho1@naver.com>
2019-06-18 00:31:11 +0900

    저장 파일명 수정

--- src/web/upload.php
+++ src/web/upload.php
@@ -2,7 +2,7 @@
     # 저장될 파일
     $uploadfile = $_FILES['upload'] ['name'];
     # 저장 경로
-    $path = "TEST/".$uploadfile;
+    $path = "TEST/test.jpg";
 
     # 임시파일 -> 저장 경로로 저장
     move_uploaded_file($_FILES['upload']['tmp_name'],$path);
